b4fdd045a6d698c956ade5dc210ac8cd03e050a8
Chuong Nguyen <quyenchuong1998@gmail.com>
2021-07-07 16:30:36 +0700

    layout improved

--- languages.js
+++ languages.js
@@ -1,8 +1,8 @@
-let languages = ["business", "entertainment", "general", "health", "science", "sports", "technology"]
+let languages = ["ar","de","en","es","fr","he","it","nl","no","pt","ru","se","ud","zh"]
 function renderCategories() {
-    let languageMap =  languages.map((language) => `<a href = "http://127.0.0.1:5501/index.html?language=${language}"> ${language} </a>`
+    let languageMap =  languages.map((language) => `<a href = "http://127.0.0.1:5501/index.html?language=${language}"> ${language.toUpperCase()} </a>`
     )
-    document.getElementById("languages").innerHTML = languageMap;
+    document.getElementById("languages").innerHTML = languageMap.join("|");
 }
 
 renderCategories();
--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 
 
 function getUrl() {
- let url = "https://newsapi.org/v2/top-headlines?apiKey=2a25c01929c345b4a768b0154c14ff98&country=us";
+ let url = "https://newsapi.org/v2/top-headlines?apiKey=2a25c01929c345b4a768b0154c14ff98";
     const urlExtension = window.location.search.split("?")[1];
-
-    if (urlExtension)
-        url += "&" + urlExtension
-    
+        if (!urlExtension) url += "&country=us"    
+        else { url += "&" + urlExtension }
     console.log(url)
     return url;
 }
@@ -23,16 +21,17 @@ async function getArticles() {
 
 function renderArticles(article) {
     return `
-        <div onmouseover="this.style.backgroundColor='beige';" onmouseout="this.style.backgroundColor='';" class="hover" style="display: flex; flex-direction: column; align-items: center; height: 80vh; width: 90vh; margin-top: 40px; border: 1px solid black">
+        <div onmouseover="this.style.backgroundColor='beige';" onmouseout="this.style.backgroundColor='';" style="display: flex; flex-direction: column; align-items: center; height: 80vh; width: 90vh; margin-top: 40px; border: 1px solid black">
             <h4 style= "width: 480px; margin-top: 15px"> ${article.title} </h4>
-           <img src="${article.urlToImage}" width = 480px>
+           <img src="${article.urlToImage}" width=480px height=240px">
            <div style="display: flex; justify-content: space-between; width:480px; margin-top: 10px" >
                 <h5> ${article.author} </h4>
                 <h6 class = "mb-0"> <a href="${article.url}">${article.source.name} </a> </h6>
             </div>
-            <p style="width:480px"> ${article.content} </p>
+            <p style="width:480px "> ${article.content} </p>
         </div>
     `
 }
+
 
 getArticles();
