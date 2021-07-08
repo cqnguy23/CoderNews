ce93e54c0424d4778ffa632e4839d6d725d3dff2
Chuong Nguyen <quyenchuong1998@gmail.com>
2021-07-08 22:00:17 +0700

    fix bugs for category section

--- languages.js
+++ languages.js
@@ -6,13 +6,16 @@ function renderLanguages() {
     if (urlExtension) {
         let tag = urlExtension.split("=")[0];
         if (tag == "category") {
-            languagesMap = languages.map((languages) => `<a href = "http://127.0.0.1:5501/index.html?language=${languages}&${urlExtension}"> ${languages.toUpperCase()} </a>`
+            languagesMap = languages.map((language) => `<a href = "http://127.0.0.1:5501/index.html?language=${language}&${urlExtension}"> ${language.toUpperCase()} </a>`
             )
+        }
+        else {
+            languagesMap = languages.map((language) => `<a href = "http://127.0.0.1:5501/index.html?language=${language}"> ${language.toUpperCase()} </a>`)
         }
     }
 
     else {
-        languagesMap = languages.map((languages) => `<a href = "http://127.0.0.1:5501/index.html?language=${languages}"> ${languages.toUpperCase()} </a>`
+        languagesMap = languages.map((language) => `<a href = "http://127.0.0.1:5501/index.html?language=${language}"> ${language.toUpperCase()} </a>`
         )
     }
     document.getElementById("languages").innerHTML = languagesMap.join("|");
